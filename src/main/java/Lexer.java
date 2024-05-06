2388a893165f94c6b2ea144db9860c6dfb6052db
rojeekoju <rojeek2@gmail.com>
2024-05-05 18:04:45 -0700

    added code

--- src/main/java/Lexer.java
+++ src/main/java/Lexer.java
@@ -6,6 +6,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+/**
+ * @author rosie
+ * Lexer class for tokenizing input text into tokens
+ */
 public class Lexer {
     private int line;
     private int pos;
@@ -15,6 +19,12 @@ public class Lexer {
 
     Map<String, TokenType> keywords = new HashMap<>();
 
+    /**
+     * @param token The tokentype of the token
+     * @param value The value associated with the token
+     * @param line The line number in the input text where the token is located
+     * @param pos The position within the line where the token starts
+     */
     static class Token {
         public TokenType tokentype;
         public String value;
@@ -23,6 +33,11 @@ public class Lexer {
         Token(TokenType token, String value, int line, int pos) {
             this.tokentype = token; this.value = value; this.line = line; this.pos = pos;
         }
+
+        /**
+         *
+         * @return string representing the Token
+         */
         @Override
         public String toString() {
             String result = String.format("%5d  %5d %-15s", this.line, this.pos, this.tokentype);
@@ -71,6 +86,8 @@ public class Lexer {
         this.keywords.put("while", TokenType.Keyword_while);
 
     }
+
+
     Token follow(char expect, TokenType ifyes, TokenType ifno, int line, int pos) {
         if (getNextChar() == expect) {
             getNextChar();
@@ -81,27 +98,135 @@ public class Lexer {
         }
         return new Token(ifno, "", line, pos);
     }
+
+    /**
+     *
+     * @param line number where the character literal is located
+     * @param pos The position within the line where the character literal starts
+     * @return A token representing the parsed character literal
+     */
     Token char_lit(int line, int pos) { // handle character literals
         char c = getNextChar(); // skip opening quote
         int n = (int)c;
-        // code here
+        //check for escape sequences
+        if (c == '\\') {
+            //if the character after '\' is 'n', treat it as a newline character
+            c = getNextChar();
+            switch (c) {
+                case 'n':
+                    n = 10; // ASCII value for newline character
+                    break;
+                case '\\':
+                    n = '\\'; // Treat '\\' as a single backslash
+                    break;
+                default:
+                    error(line, pos, String.format("unknown escape sequence \\%c", c));
+                    break;
+            }
+        }
+        // Ensure there's only one character in the literal, which should be the closing quote
+        if (c != '\'') {
+            error(line, pos, "multi-character constant");
+        }
+        //Move past the closing quote
+        getNextChar();
         return new Token(TokenType.Integer, "" + n, line, pos);
     }
+
+    /**
+     *
+     * @param start The Character marks the start of string literal
+     * @param line The line number where the string  literal is located
+     * @param pos The position within the line where the string literal starts
+     * @return A token represented the parsed string literal
+     */
     Token string_lit(char start, int line, int pos) { // handle string literals
         String result = "";
-        // code here
+        while (getNextChar() != start) {
+            //check for null character, indicating an unexpected end of file
+            if (this.chr == '\u0000') {
+                error(line, pos, "End of file");
+            }
+            //check for newline character
+            if (this.chr == '\n') {
+                error(line, pos, "End of file");
+            }
+            result += this.chr;
+        }
+        //Move past the start character to prepare for next token
+        getNextChar();
         return new Token(TokenType.String, result, line, pos);
     }
+
+    /**
+     *
+     * @param line The line number where the division operator or comment starts
+     * @param pos The position within th line where the division operator or comment starts
+     * @return A token that represents either a division operator or the end of a comment
+     */
     Token div_or_comment(int line, int pos) { // handle division or comments
         // code here
-        return getToken();
+        char currentChar = getNextChar(); //Get the next character
+        //If the next character is not '*', it's a division operator
+        if (currentChar != '*') {
+            return new Token(TokenType.Op_divide, "", line, pos);
+        }
+        //Move to the next character
+        getNextChar();
+
+        //check for comments
+        while (true) {
+            //If the current character is null(end of file), raise an error
+            if (this.chr == '\u0000') {
+                error(line, pos, "End of file in comment");
+
+                //if the current character is '*', check if it's part of the end of a comment
+            } else if (this.chr == '*') {
+                //Move to the next character
+                char nextChar = getNextChar();
+                //If the next character is '/', the comment ends
+                if (getNextChar() == '/') {
+                    //Move the end of comment and return the next token
+                    getNextChar();
+                    return getToken();
+                }
+                //Move to the next character if it's not the end of a comment
+            } else {
+                currentChar = getNextChar();
+            }
+        }
     }
+
+    /**
+     *
+     * @param line The line number where the identifier or integer starts
+     * @param pos The position within the line where the identifier or integer starts
+     * @return A token representing either an identifier, integer or keyword
+     */
     Token identifier_or_integer(int line, int pos) { // handle identifiers and integers
         boolean is_number = true;
         String text = "";
-        // code here
+        while (Character.isAlphabetic(this.chr) || Character.isDigit(this.chr)) {
+        text += this.chr; //Add character to the text
+
+            if (!Character.isDigit(this.chr)){
+                is_number = false; //if it's not a digit, it's not a number
+            }
+            getNextChar();
+
+        }
+        if (Character.isDigit(text.charAt(0)) && is_number) {
+            return new Token(TokenType.Integer, text, line, pos);
+        }
+        //check if it's a keyword
+        if(this.keywords.containsKey(text)) {
+            return new Token(this.keywords.get(text), "", line, pos); //created a toke for the keyword
+        }
+        //if not a number or keyword, return as in identifier
         return new Token(TokenType.Identifier, text, line, pos);
     }
+
+
     Token getToken() {
         int line, pos;
         while (Character.isWhitespace(this.chr)) {
@@ -114,11 +239,98 @@ public class Lexer {
 
         switch (this.chr) {
             case '\u0000': return new Token(TokenType.End_of_input, "", this.line, this.pos);
-            // remaining case statements
 
-            default: return identifier_or_integer(line, pos);
+            case '(':
+                chr = getNextChar();
+                return new Token(TokenType.LeftParen, "", line, pos);
+            case ')':
+                chr = getNextChar();
+                return new Token(TokenType.RightParen, "", line, pos);
+            case '{' :
+                chr = getNextChar();
+                return new Token(TokenType.LeftBrace, "", line, pos);
+            case'}' :
+                chr = getNextChar();
+                return new Token(TokenType.RightBrace, "", line, pos);
+            case ';':
+                chr = getNextChar();
+                return new Token(TokenType.Semicolon, "", line, pos);
+            case ',':
+                chr = getNextChar();
+                return new Token(TokenType.Comma, "", line, pos);
+
+            case '*':
+                chr = getNextChar();
+                return new Token(TokenType.Op_multiply, "", line, pos);
+            case '/':
+                chr = getNextChar();
+               return div_or_comment(line, pos);
+            case '%' :
+                chr = getNextChar();
+                return new Token(TokenType.Op_mod, "", line,pos);
+            case'+' :
+                chr = getNextChar();
+                return new Token(TokenType.Op_add, "", line,pos);
+            case '-':
+                chr = getNextChar();
+                return new Token(TokenType.Op_subtract, "", line, pos);
+            case '<':
+                chr = getNextChar();
+                if(chr != '='){
+                    return  new Token(TokenType.Op_less, "", line, pos);
+                }
+                else {
+                    return  new Token(TokenType.Op_lessequal, "", line, pos);
+                }
+
+            case '>':
+                chr = getNextChar();
+                if (chr != '=') {
+                    return new Token(TokenType.Op_greater, "",line,pos);
+                }
+                else {
+                    return  new Token(TokenType.Op_greaterequal, "", line, pos);
+                }
+            case '!':
+                chr = getNextChar();
+                if (chr != '=') {
+                    return new Token(TokenType.Op_not, "", line,pos);
+                }
+                else {
+                    return  new Token(TokenType.Op_notequal, "", line, pos);
+                }
+            case '=':
+                chr = getNextChar();
+                if (chr != '=') {
+                    return new Token(TokenType.Op_assign, "", line, pos);
+                }
+                else {
+                    return  new Token(TokenType.Op_equal, "", line, pos);
+                }
+                case '&':
+                chr = getNextChar();
+                if (chr == '&') {
+                    return new Token(TokenType.Op_and, "", line, pos);
+                }
+                else {
+                    return char_lit(line, pos);
+                }
+            case '|':
+                chr = getNextChar();
+                if (chr == '|') {
+                    return new Token(TokenType.Op_or, "", line,pos);
+                }
+                else {
+                    return char_lit(line, pos);
+                }
+            case '"':
+
+                return string_lit(this.chr, line, pos);
+
+                default: return identifier_or_integer(line, pos);
         }
     }
+
 
     char getNextChar() {
         this.pos++;
@@ -134,6 +346,7 @@ public class Lexer {
         }
         return this.chr;
     }
+
 
     String printTokens() {
         Token t;
